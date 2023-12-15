20f9d3de78ce053aa433c34de6605c7329d278a3
albetnov <ryxinfrvr@gmail.com>
2023-12-15 14:54:27 +0700

    chore: remove unused `module` from contract

--- modules/stock_movement/stock_movement.contract.go
+++ modules/stock_movement/stock_movement.contract.go
@@ -2,6 +2,5 @@ package stock_movement
 
 type StockMovement struct {
 	EarningId uint   `json:"earning_id" binding:"required,number"`
-	Module    string `json:"module" binding:"required,oneof=earnings"`
 	Desc      string `json:"desc" binding:"required,max=225"`
 }
